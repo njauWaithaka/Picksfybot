3e3803263acfe69784c7947456f932a362654631
unknown <njaualex20@gmail.com>
2023-02-07 01:17:55 +0300

    Telegram update function stub

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,5 @@
-exports.handler = async(event) => {
-    console.log("Received an update from Telegram!", event.body);
-    return { statusCode: 200 };
-};
+ console.log('now')
+ exports.handler = async(event) => {
+     console.log("Received an update from Telegram!", event.body);
+     return { statusCode: 200 };
+ };
