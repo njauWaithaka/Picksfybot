955a6391d8bbcbab97b81a203bda82f283d605d1
njauWaithaka <83164235+njauWaithaka@users.noreply.github.com>
2023-02-13 19:35:28 +0300

    Update update.js
    Update js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -16,7 +16,8 @@ bot.start(ctx => {
 exports.handler = async event => {
   try {
     await bot.handleUpdate(JSON.parse(event.body))
-    return { statusCode: 200, body: "" }
+    return { statusCode: 200, body: "" } 
+    
   } catch (e) {
     console.error("error in handler:", e)
     return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
