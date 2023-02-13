ba1424be2ef50c9be1302e83b9acd0e2999e313f
njauWaithaka <83164235+njauWaithaka@users.noreply.github.com>
2023-02-13 19:42:32 +0300

    Update

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf")
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN) 
 
 bot.start(ctx => {
   console.log("Received /start command")
