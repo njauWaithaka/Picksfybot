0573d406c307701e9b2e661491980be09d0f2a1a
njauWaithaka <83164235+njauWaithaka@users.noreply.github.com>
2023-02-13 15:26:47 +0300

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,4 @@
-await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-  chat_id: JSON.parse(event.body).message.chat.id,
-  text: "I got your message!",
-});
+exports.handler = async (event) => {
+  console.log("Received an update from Telegram!", event.body);
+  return { statusCode: 200 };
+};
